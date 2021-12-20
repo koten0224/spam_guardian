d2d5b451bd163b6074738c989bff0fe31396deb6
Koten <koten0224@gmail.com>
2021-12-21 03:19:22 +0800

    feat(params builder): add comment

--- lib/spam_guardian/params_builder.rb
+++ lib/spam_guardian/params_builder.rb
@@ -1,5 +1,12 @@
 module SpamGuardian
   class ParamsBuilder < ActiveSupport::HashWithIndifferentAccess
+    # Support string
+    # support nth array 
+    # Support nth array of string
+    # Support nth array of hash of string
+    # Support nth array of hash of array
+    # Support hash of string
+    # Support hash of array of string
     ALLOWED_KEYS = %i(email ip username).freeze
     attr_reader :temp_params
     def initialize(params={})
@@ -16,6 +23,7 @@ module SpamGuardian
       end
     end
 
+    private
     def validate_hash_keys!(hash)
       hash.each_key do |key|
         raise FormatError.new("#{key}: invalid key.") unless key.to_sym.in?(ALLOWED_KEYS)
